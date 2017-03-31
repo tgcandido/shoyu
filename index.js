00521192cbe8254b5545084e300f5337859183f6
tgcandido <thiago.gcandido@gmail.com>
2017-03-31 20:43:51 -0300

    adding lsresult parsing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const args = require('args')
 const installer = require('./lib/installer')
 
+//installer()
 //TODO add commands
 args.command('install', 'parse and install dependencies', installer)
 
