fae021f4d38d8a05708381696663de11129f340a
tgcandido <thiago.gcandido@gmail.com>
2017-04-01 12:19:28 -0300

    adding package installation

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const args = require('args')
 const installer = require('./lib/installer')
 
-//installer()
-//TODO add commands
 args.command('install', 'parse and install dependencies', installer)
 
 const flags = args.parse(process.argv)
